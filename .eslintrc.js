10bc9bed5e4cf0c6ac2cf74491c8d279a2dba08e
Gavin Birkhoff <gavinbirkhoff@gmail.com>
2021-06-14 22:41:55 +0800

    Configure integrated environment

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {}
+}
---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'none',
+  bracketSpacing: true,
+  printWidth: 120,
+  tabWidth: 2,
+  arrowParens: 'always',
+  endOfLine: 'auto',
+  quoteProps: 'consistent'
+}
