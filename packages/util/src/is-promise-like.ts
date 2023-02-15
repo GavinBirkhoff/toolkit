02b2e45bb9fbba5da181a3fc9d1c4f1fc3c16957
Gavin Birkhoff <gavinbirkhoff@gmail.com>
2023-02-15 20:22:51 +0800

    add promise A+

--- packages/util/src/is-promise-like.ts
+++ packages/util/src/is-promise-like.ts
@@ -3,7 +3,7 @@
  * @param {*} obj Accept an type
  * @returns {boolean}
  */
-
+// Promises/A+ https://promisesaplus.com/
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types, @typescript-eslint/no-explicit-any
 const isPromiseLike = (obj: any): boolean =>
   obj !== null && (typeof obj === 'object' || typeof obj === 'function') && typeof obj.then === 'function'
