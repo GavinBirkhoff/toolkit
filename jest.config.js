052a4e5e5cde626d25338f6b65e817ff4f87be94
Gavin Birkhoff <gavinbirkhoff@gmail.com>
2023-02-17 23:46:35 +0800

    add test enviroment  of jsdom

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,6 @@ module.exports = {
     '^.+\\.tsx?$': 'ts-jest'
   },
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testEnvironment: 'jsdom'
 }
