07121e5d9e43bb1151d4054e397e0b9e40ea438e
Gavin Birkhoff <gavinbirkhoff@gmail.com>
2023-02-17 23:51:11 +0800

    Update getTextPixelWith.ts

--- packages/dom-util/src/getTextPixelWith.ts
+++ packages/dom-util/src/getTextPixelWith.ts
@@ -13,7 +13,7 @@ interface Ifont {
  */
 const getTextPixelWith = (text: string, font: Ifont): number => {
   const { fontStyle, fontVariant, fontWeight, fontSize = '16px', fontFamily = 'Arial' } = font
-  const ctx = document.createElement('canvas').getContext('2d')
+  const ctx = document.createElement('canvas')?.getContext('2d')
   if (ctx === null) return 0
   ctx.font = `${fontStyle !== undefined ? fontStyle : ''} ${fontVariant !== undefined ? fontVariant : ''} ${
     fontWeight !== undefined ? fontWeight : ''
