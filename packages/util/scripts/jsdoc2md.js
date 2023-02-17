1ea6ac46aadd0774b52999bb48efe0223f2ed579
Gavin Birkhoff <gavinbirkhoff@gmail.com>
2023-02-17 23:45:00 +0800

    remove log

--- packages/util/scripts/jsdoc2md.js
+++ packages/util/scripts/jsdoc2md.js
@@ -1,5 +1,4 @@
 const path = require('path')
 const jsdoc2md = require('jsdoc-to-markdown')
-console.log(111111111111)
 
 jsdoc2md.render({ files: path.join(__dirname, '..', 'esm/*.js') }).then(console.log)
