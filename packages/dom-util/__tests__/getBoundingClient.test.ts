ae22636753e20430aee36c44928255d6e450516f
Gavin Birkhoff <gavinbirkhoff@gmail.com>
2023-02-17 23:47:41 +0800

    Update getBoundingClient.test.ts

--- packages/dom-util/__tests__/getBoundingClient.test.ts
+++ packages/dom-util/__tests__/getBoundingClient.test.ts
@@ -1,10 +1,10 @@
-import { getBoundingClient, createDom, setCssProps } from '../src'
+import { getBoundingClient, createDom, setStyleProps } from '../src'
 describe('getBoundingClient', () => {
   test('default', () => {
     const div = createDom({ tagName: 'div', container: document.body })
-    setCssProps(div, { width: '200px', padding: '100px', margin: '100px' })
+    setStyleProps(div, { width: '200px', padding: '100px', margin: '100px' })
     const div2 = createDom({ tagName: 'div', container: div })
-    setCssProps(div2, { width: '200px', padding: '100px' })
+    setStyleProps(div2, { width: '200px', padding: '100px' })
     //TODO I think it's not computer then the result is 0
     expect(getBoundingClient(div2)).toEqual({ x: 0, y: 0 })
   })
