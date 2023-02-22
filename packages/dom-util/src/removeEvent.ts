e3f19c0384df2a66df5a907ba64f4a24211a6166
Gavin Birkhoff <gavinbirkhoff@gmail.com>
2023-02-22 20:37:44 +0800

    Create removeEvent.ts

---
+++ packages/dom-util/src/removeEvent.ts
@@ -0,0 +1,27 @@
+/**
+ * @description Remove event from target element
+ * @param ele target element
+ * @param type type of event
+ * @param callBack listener callback
+ * @param useCapture capture default false
+ * @returns {void}
+ * @example
+ * const callBack = addEvent(element, 'click', ()=>{})
+ * removeEvent(element, 'click', callBack)
+ */
+
+const removeEvent = (ele: HTMLElement | SVGElement, type: string, callBack: any, useCapture = false): void => {
+  if (!ele) return
+  if (ele.removeEventListener) {
+    ele.removeEventListener(type, callBack, useCapture)
+    //@ts-ignore
+  } else if (ele.detachEvent) {
+    //@ts-ignore
+    ele.detachEvent('on' + type, callBack)
+  } else {
+    //@ts-ignore
+    ele['on' + type] = null
+  }
+}
+
+export default removeEvent
