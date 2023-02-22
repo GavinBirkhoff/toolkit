74374d52e0f69c26e74d136085ef26aa492078a1
Gavin Birkhoff <gavinbirkhoff@gmail.com>
2023-02-22 20:37:03 +0800

    Update formatNumber

--- packages/util/__tests__/formatNumber.test.ts
+++ packages/util/__tests__/formatNumber.test.ts
@@ -1,9 +1,15 @@
 import { formatNumber } from '../src'
-describe('formatNumber2', () => {
-   test(' should be 1,000.00', () => {
-      expect(formatNumber(1000, ',', 2)).toBe('1,000.00')
-   })
-   test(' should be 10', () => {
-      expect(formatNumber(10, ',', 1)).toBe('10.0')
-   })
+describe('formatNumber', () => {
+  test(' should be 1,000.00', () => {
+    expect(formatNumber(1000, ',', 2)).toBe('1,000.00')
+  })
+  test(' should be 1,000,000.00', () => {
+    expect(formatNumber(1000000, ',', 2)).toBe('1,000,000.00')
+  })
+  test(' should be 10.0', () => {
+    expect(formatNumber(10, ',', 1)).toBe('10.0')
+  })
+  test(' should be 10', () => {
+    expect(formatNumber(10, ',')).toBe('10')
+  })
 })
--- packages/util/src/format-number.ts
+++ packages/util/src/format-number.ts
@@ -1,40 +1,44 @@
 /**
  * @description JS digit thousandth formatting code
- * @param value Numeric value to format
- * @param separator Thousandth sign
- * @param digitNum Keep several decimal places
- * @returns {string | number}
+ * @param {number} value Numeric value to format
+ * @param {string} separator Thousandth sign
+ * @param {number} digitNum Keep several decimal places
+ * @returns {string}
+ * @example
+ * formatNumber(1000, ',', 2) => '1,000.00'
+ * formatNumber(10, ',', 1) => '10.0'
+ * formatNumber(10, ',') => '10'
  */
-const formatNumber2 = (value: number | string, separator: string, digitNum = 0): string | number => {
-   const initV = value
+const formatNumber = (val: number, separator: string, digitNum = 0): string => {
+  let value: any = val
 
-   if ((value = (value = value + '').replace(/^\s*|\s*$|,*/g, '')).match(/^\d*\.?\d*$/) == null) return initV
+  if ((value = (value = value + '').replace(/^\s*|\s*$|,*/g, '')).match(/^\d*\.?\d*$/) == null) return val.toString()
 
-   value = (digitNum as number) >= 0 ? Number(value).toFixed(digitNum) + '' : value
+  value = (digitNum as number) >= 0 ? Number(value).toFixed(digitNum) + '' : value
 
-   let r: string[] | string = [],
-      tl = value.split('.')[0],
-      tr = value.split('.')[1]
+  let r: string[] | string = [],
+    tl = value.split('.')[0],
+    tr = value.split('.')[1]
 
-   tr = typeof tr != 'undefined' ? tr : ''
+  tr = typeof tr != 'undefined' ? tr : ''
 
-   if (separator != null && separator != '') {
-      while (tl.length >= 3) {
-         r.push(tl.substring(tl.length - 3))
+  if (separator != null && separator != '') {
+    while (tl.length >= 3) {
+      r.push(tl.substring(tl.length - 3))
 
-         tl = tl.substring(0, tl.length - 3)
-      }
+      tl = tl.substring(0, tl.length - 3)
+    }
 
-      if (tl.length > 0) r.push(tl)
+    if (tl.length > 0) r.push(tl)
 
-      r.reverse()
+    r.reverse()
 
-      r = r.join(separator)
+    r = r.join(separator)
 
-      return tr == '' ? r : r + '.' + tr
-   }
+    return tr == '' ? r : r + '.' + tr
+  }
 
-   return value
+  return value as string
 }
 
-export default formatNumber2
+export default formatNumber
