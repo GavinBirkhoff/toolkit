02cb4805b4f6e9e820f471e8e8dbc330f1cef8cc
Gavin Birkhoff <gavinbirkhoff@gmail.com>
2023-02-22 20:35:47 +0800

    Create downloadByBlob.ts

---
+++ packages/dom-util/src/downloadByBlob.ts
@@ -0,0 +1,17 @@
+/**
+ * @description Download resources thought Blob
+ * @param {BlobPart} dataStream
+ * @param {string} fileName
+ */
+const downloadByBlob = (dataStream: BlobPart, fileName = 'unknown'): void => {
+  const blob = new Blob([dataStream])
+  const objectURL = URL.createObjectURL(blob)
+  let btn = document.createElement('a')
+  btn.download = `${fileName}.xlsx` //file type
+  btn.href = objectURL
+  btn.click()
+  URL.revokeObjectURL(objectURL)
+  btn = null as any
+}
+
+export default downloadByBlob
