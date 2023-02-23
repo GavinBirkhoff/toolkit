72752c7d03f99663ba0d969ec9ce2370522f29b7
Gavin Birkhoff <gavinbirkhoff@gmail.com>
2023-02-23 20:23:43 +0800

    Update createDom.ts

--- packages/dom-util/src/createDom.ts
+++ packages/dom-util/src/createDom.ts
@@ -1,17 +1,25 @@
 import { isElement, isString } from 'util4j'
-type DomAttrs = {
-  tagName?: string
+
+export interface DomAttrs<T> {
+  tagName: T
   container?: HTMLElement
   node?: HTMLElement | string
 }
+
 /**
- * create a dom
+ * @description Create a dom
  * @param {DomAttrs} domAttrs tagName create tag name, container parent node,node child node
- * @returns {HTMLElement}
+ * @returns {HTMLElementTagNameMap[K]}
+ * @example
+ * // returns <div></div>
+ * createDom()
+ * @example
+ * / returns <body><a>link</a></body>
+ * createDom({tagName:'a', container:body, node: 'link'})
  */
-const createDom = (domAttrs: DomAttrs = {}): HTMLElement => {
-  const { tagName = 'div', container, node } = domAttrs
-  const el = document.createElement(tagName)
+const createDom = <K extends keyof HTMLElementTagNameMap>(domAttrs: DomAttrs<K>): HTMLElementTagNameMap[K] => {
+  const { tagName, container, node } = domAttrs
+  const el = document.createElement(tagName ?? 'div')
   if (node) {
     if (isString(node)) {
       const textNode = document.createTextNode(node as string)
