dc9eb4ef6bdea8c1c67602eea88a4f6fcab22a0a
Gavin Birkhoff <gavinbirkhoff@gmail.com>
2023-02-24 23:15:03 +0800

    test curry

--- packages/util/__tests__/curry.test.ts
+++ packages/util/__tests__/curry.test.ts
@@ -1,6 +1,9 @@
 import { curry, add } from '../src'
 describe('curry', () => {
-   test('1 + 2 =3', () => {
-      expect(curry(add)(1, 2)).toBe(3)
-   })
+  test('1 + 2 =3', () => {
+    expect(curry(add)(1, 2)).toBe(3)
+  })
+  test('1 + 2 =3', () => {
+    expect(curry(add, 1)(2)).toBe(3)
+  })
 })
--- packages/util/src/curry.ts
+++ packages/util/src/curry.ts
@@ -1,5 +1,5 @@
 /**
- * @param  {(...rest:any[])=>any} fn
+ * @param  {Function} fn
  * @param  {any[]} ...rest
  * @returns {((...rest: any[]) => any)}
  */
@@ -7,10 +7,10 @@ const curry = (fn: (...rest: any[]) => any, ...rest: any[]): ((...rest: any[]) =
   // const slice = Array.prototype.slice,
   //   stored_args = slice.call(arguments, 1)
   const stored_args = [...rest]
-  return (...rest: any[]) => {
+  return (...subRest: any[]) => {
     // const new_args = slice.call(arguments),
     //   args = stored_args.concat(new_args)
-    const new_args = [...rest]
+    const new_args = [...subRest]
     const args = stored_args.concat(new_args)
     // return fn.apply(null, args)
     return fn(...args)
