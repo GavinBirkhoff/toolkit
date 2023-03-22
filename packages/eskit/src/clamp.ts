b9a6dceeaaeed64c9044b429428bb3ae84af4027
Gavin Birkhoff <gavinbirkhoff@gmail.com>
2023-03-22 22:15:32 +0800

    Reconstruct the clamp function

--- packages/eskit/src/clamp.ts
+++ packages/eskit/src/clamp.ts
@@ -1,23 +1,21 @@
 /**
- * Put a num in a certain boundary
- * @param {number} num The value to clamp
- * @param {number} min The min bound
- * @param {number} max The max bound
- * @returns {number}
+ * Limits a number to be within a certain range.
+ *
+ * @param {number} num - The number to clamp.
+ * @param {number} min - The lower boundary of the range.
+ * @param {number} max - The upper boundary of the range.
+ * @returns {number} - The clamped number.
  * @since 1.0.0
+ *
  * @example
- * // returns -5
- * clamp(-10, -5, 5)
+ * // Returns -5
+ * clamp(-10, -5, 5);
+ *
  * @example
- * // returns 5
- * clamp(10, -5, 5)
+ * // Returns 5
+ * clamp(10, -5, 5);
  */
 const clamp = (num: number, min: number, max: number): number => {
-  if (num < min) {
-    return min
-  } else if (num > max) {
-    return max
-  }
-  return num
+  return num < min ? min : num > max ? max : num
 }
 export default clamp
