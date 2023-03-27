6775686362ff9c48e5f945d5cb751ee920869090
Gavin Birkhoff <gavinbirkhoff@gmail.com>
2023-03-27 22:53:53 +0800

    Reconstruct hasClass function

--- packages/domkit/__tests__/getClass.test.ts
+++ packages/domkit/__tests__/getClass.test.ts
@@ -1,9 +1,26 @@
 import { getClass } from '../src'
 describe('getClass', () => {
-  test('default', () => {
-    const dom = document.createElement('div')
-    dom.classList.add('wrap')
-    dom.classList.add('box')
-    expect(getClass(dom)).toBe('wrap box')
+  it('should return class names on SVG Element', () => {
+    const svg: SVGElement = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
+    svg.setAttribute('class', 'example-class-1 example-class-2')
+    expect(getClass(svg)).toBe('example-class-1 example-class-2')
+  })
+
+  it('should return class names on HTMLElement', () => {
+    const div: HTMLDivElement = document.createElement('div')
+    div.setAttribute('class', 'example-class-1 example-class-2')
+    expect(getClass(div)).toBe('example-class-1 example-class-2')
+  })
+
+  it('should return empty string when no classes exist on element', () => {
+    const div: HTMLDivElement = document.createElement('div')
+    expect(getClass(div)).toBe('')
+  })
+
+  it('should return class names for HTML elements using classList', () => {
+    const div: HTMLDivElement = document.createElement('div')
+    div.classList.add('example-class-1')
+    div.classList.add('example-class-2')
+    expect(getClass(div)).toBe('example-class-1 example-class-2')
   })
 })
--- packages/domkit/src/getClass.ts
+++ packages/domkit/src/getClass.ts
@@ -1,13 +1,22 @@
 /**
- * Get classnames
- * @param {HTMLElement} el target element
- * @returns {string} class name
- * @since 1.0.0
+ * Returns a string of all the CSS class names assigned to an element, including SVG elements.
+ *
+ * @param el - The element to retrieve the class names from
+ * @returns A string containing class names
+ *
  * @example
- * getClass(el) => className
+ *
+ * const el = document.querySelector('.example-class')!;
+ * const classes = getClass(el);
+ * console.log(classes); // 'example-class'
+ *
+ * @since 1.0.0
  */
-const getClass = (el: HTMLElement | SVGElement): string => {
-  return el.className.baseVal === undefined ? el.className : el.className.baseVal
+export function getClass(el: HTMLElement | SVGElement): string {
+  if (el instanceof SVGElement) {
+    return el.getAttribute('class') || ''
+  }
+  return 'className' in el ? el.className : (el as HTMLElement | SVGElement).getAttribute('class') || ''
 }
 
 export default getClass
