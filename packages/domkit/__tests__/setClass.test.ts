1ba3da4061c66760ff6a41614c2b28b36ad3feb3
Gavin Birkhoff <gavinbirkhoff@gmail.com>
2023-03-27 22:05:02 +0800

    Reconstruct setClass function

--- packages/domkit/__tests__/setClass.test.ts
+++ packages/domkit/__tests__/setClass.test.ts
@@ -1,10 +1,40 @@
-import { setClass, createDom, addClass, getClass } from '../src'
+import { setClass } from '../src'
+
+import { JSDOM } from 'jsdom'
 
 describe('setClass', () => {
-  test('default', () => {
-    const dom = createDom({ tagName: 'div' })
-    addClass(dom, 'foo')
-    setClass(dom, 'boo')
-    expect(getClass(dom)).toBe('boo')
+  let dom: JSDOM
+  let el: HTMLElement | SVGElement
+
+  beforeEach(() => {
+    dom = new JSDOM('<html><body></body></html>')
+    el = dom.window.document.createElement('div')
+  })
+
+  afterEach(() => {
+    dom.window.document.body.innerHTML = ''
+    dom.window.close()
+  })
+
+  it('should set the class attribute for an HTML element', () => {
+    setClass(el, 'my-class')
+    expect(el.getAttribute('class')).toBe('my-class')
+  })
+
+  it('should set the class attribute for an SVG element', () => {
+    el = dom.window.document.createElementNS('http://www.w3.org/2000/svg', 'circle')
+    setClass(el, 'my-class')
+    expect(el.getAttribute('class')).toBe('my-class')
+  })
+
+  it('should set the className property for an HTML element', () => {
+    setClass(el, 'my-class')
+    expect(el.classList.contains('my-class')).toBe(true)
+  })
+
+  it('should set the className property for an SVG element', () => {
+    el = dom.window.document.createElementNS('http://www.w3.org/2000/svg', 'circle')
+    setClass(el, 'my-class')
+    expect(el.classList.contains('my-class')).toBe(true)
   })
 })
--- packages/domkit/src/setClass.ts
+++ packages/domkit/src/setClass.ts
@@ -1,18 +1,25 @@
 /**
- * Set class name, but overwrite the previous class name
- * @param {HTMLElement} el target dom
- * @param {string} name The value of class
- * @since 1.0.0
+ * Sets the class attribute or className property of an element to the specified value.
+ * @param el The element to set the class attribute/property for.
+ * @param className The class name(s) to set on the element.
  * @example
- * setClass(el, 'name')
- * setClass(el, 'name1 name2')
+ *
+ * const myEl = document.getElementById('my-element')!;
+ * setClass(myEl, 'my-class');
+ * // The above call sets the class to "my-class" on #my-element.
+ *
+ * @since
  */
-const setClass = (el: HTMLElement | SVGElement, name: string): void => {
-  if (el.className.baseVal === undefined) {
-    //@ts-ignore
-    el.className = name
+export const setClass = (el: HTMLElement | SVGElement, className: string): void => {
+  if ('classList' in el) {
+    el.setAttribute('class', className)
+  } else if ('className' in el) {
+    ;(el as HTMLElement | SVGElement).setAttribute('className', className)
   } else {
-    el.className.baseVal = name
+    // for ancient browsers
+    ;(el as HTMLElement | SVGElement).setAttribute('class', className)
+    ;(el as HTMLElement | SVGElement).setAttribute('className', className)
   }
 }
+
 export default setClass
