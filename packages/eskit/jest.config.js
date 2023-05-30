f8288d38f77d73894466aa68a9588138230d8b74
Gavin Birkhoff <gavinbirkhoff@gmail.com>
2023-05-30 22:22:34 +0800

    Update eskit jest

--- packages/eskit/jest.config.js
+++ packages/eskit/jest.config.js
@@ -1,10 +1,10 @@
-export default {
+module.exports = {
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
-    '\\.[j]sx?$': 'babel-jest',
+    '\\.[j]sx?$': 'babel-jest'
   },
   testRegex: '(/__test__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
   // collectCoverageFrom: ['src/**/!(*.d).{js,ts}'],
   // reporters: [
   //   'default',
@@ -16,4 +16,4 @@ export default {
   //     }
   //   ]
   // ]
-};
+}
